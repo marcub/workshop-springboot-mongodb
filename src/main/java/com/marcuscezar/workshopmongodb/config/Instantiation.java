3aaec34a5f5ead8af2320ddfd34e0210d42840f9
Marcus Cezar <marcusferraz98@gmail.com>
2023-02-08 22:18:55 -0300

    Created comments and added to posts

--- src/main/java/com/marcuscezar/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/marcuscezar/workshopmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.marcuscezar.workshopmongodb.config;
 import com.marcuscezar.workshopmongodb.domain.Post;
 import com.marcuscezar.workshopmongodb.domain.User;
 import com.marcuscezar.workshopmongodb.dto.AuthorDTO;
+import com.marcuscezar.workshopmongodb.dto.CommentDTO;
 import com.marcuscezar.workshopmongodb.repository.PostRepository;
 import com.marcuscezar.workshopmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, format.parse("12/01/2022"), "Partiu trilha!", "Trilha muito interessante em Maceió", new AuthorDTO(maria));
         Post post2 = new Post(null, format.parse("02/06/2021"), "São João chegando!", "Gosto demais dessa época", new AuthorDTO(maria));
+
+        CommentDTO comment1 = new CommentDTO("Top demais a trilha!", format.parse("21/03/2022"), new AuthorDTO(alex));
+        CommentDTO comment2 = new CommentDTO("Aproveitou?!", format.parse("22/03/2022"), new AuthorDTO(bob));
+        CommentDTO comment3 = new CommentDTO("Muito boa mesmo!", format.parse("02/06/2021"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(comment1, comment2));
+        post2.getComments().add(comment3);
 
         postRepository.saveAll(Arrays.asList(post1,post2));
 
--- src/main/java/com/marcuscezar/workshopmongodb/domain/Post.java
+++ src/main/java/com/marcuscezar/workshopmongodb/domain/Post.java
@@ -1,11 +1,16 @@
 package com.marcuscezar.workshopmongodb.domain;
 
 import com.marcuscezar.workshopmongodb.dto.AuthorDTO;
+import com.marcuscezar.workshopmongodb.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.LazyLoadingException;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "post")
@@ -17,6 +22,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -67,6 +74,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
